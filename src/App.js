99d4ca10b488364a15019cae7e9ffa70ac691885
DaianeCH <cristinepenaforte@gmail.com>
2023-03-24 21:29:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const Container = styled.section `
+  background: #FFEBB4;
+  padding: 20px;
+  button{
+    background-color: #FFACAC;
+    color: black;
+    border: none;
+    padding: 10px;
+    margin-top: 10px;
+    cursor: pointer;
+    font-weight: 800;
+  }
+
+  h1{
+    color: black;
+    margin-bottom: 20px;
+  }
+
+  input {
+    padding: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    border: none;
+    margin-right: 10px;
+    width: 300px;
+  }
+
+  ol li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  `;
+
+
+// To Do
+export default class App extends React.Component {
+  state = {
+    tarefa: "",
+    lista: [],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      tarefa: e.target.value
+    });
+  };
+
+  addTarefa = () => {
+    this.setState({
+      lista: this.state.lista.concat({
+        tarefa: this.state.tarefa
+      }),
+      // Apagando o input depois que envia
+      tarefa: ""
+    });
+  };
+
+  delete = (index) => {
+    this.setState({
+      lista: this.state.lista.filter((item, i) => i !== index),
+    });
+  };
+
+
+  render() {
+    return (
+      <>
+        <Container>
+          <h1>To Do List - Lista de Tarefas</h1>
+          <input value={this.state.tarefa} onChange={this.handleChange}/>
+          <button onClick={this.addTarefa}>ADICIONAR TAREFA</button>
+          <ol>
+            {this.state.lista.map((item, index) => (
+              <li>
+                {item.tarefa}
+                <button onClick={() => this.delete(index)}>x</button>
+              </li>
+            ))}
+          </ol>
+        </Container>
+      </>
+    );
+  }
 }
-
-export default App;
